05f2bc9dbe5105c8520d326d48d04f1384433d6d
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2021-07-21 22:25:46 +0800

    inheritance example

---
+++ inheritance.cpp
@@ -0,0 +1,40 @@
+/*
+    Develped by Nay Oo Kyaw
+    nayookyaw.nok@gmail.com
+*/
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+    protected:
+        string brand = "BMW";
+    
+    public:
+        void showVehicle() {
+            cout << "Show vechile" << "\n";
+        }
+
+        string getBrand() {
+            return brand;
+        }
+
+};
+
+class Car: public Vehicle {
+    private:
+        string model = "X-20";
+    
+    public:
+        void showBrand() {
+            cout << brand << endl;
+        }
+};
+
+int main() {
+    Car car;
+    car.showVehicle();
+
+    car.showBrand();
+
+    return 0;
+}
